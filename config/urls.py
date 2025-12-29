e641b453c7248c262ac860bf9253c2b5838fcb40
maxsudbekjon <toshpulatovm73@gmail.com>
2025-12-29 09:31:54 +0500

    Initial commit

--- config/urls.py
+++ config/urls.py
@@ -36,7 +36,7 @@ urlpatterns += i18n_patterns(
 # Swagger and ReDoc URLs
 urlpatterns += [
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
