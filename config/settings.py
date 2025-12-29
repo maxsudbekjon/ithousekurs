62bdc7370ec66d6288df8f16643e70d5bc25837d
maxsudbekjon <toshpulatovm73@gmail.com>
2025-12-29 08:07:49 +0500

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,7 @@ PAYTECHUZ = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -84,7 +85,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
