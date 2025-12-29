2799f4fdf1b9a59bad9f7088535871c3b86a0399
maxsudbekjon <toshpulatovm73@gmail.com>
2025-12-29 08:28:49 +0500

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -191,29 +191,23 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+# Ushbu list keyinchalik cheklangan rejimga qaytishda ishlatish uchun qoldirildi
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://45.130.104.72:8014",
+    "https://45.130.104.72:8014",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://45.130.104.72:8014",
+    "https://45.130.104.72:8014",
 ]
-
-CORS_ALLOW_METHODS = (
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-)
 
 
 SIMPLE_JWT = {
@@ -277,5 +271,4 @@ JAZZMIN_SETTINGS = {
     "site_brand": "It House",
     "welcome_sign": "It House Admin"
 }
-
 
